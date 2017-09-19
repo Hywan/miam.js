1b1b68cdc02d09ff6874e8bffaa3a1ee6c458b83
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2017-09-19 01:30:41 -0700

    test(parser) Add test suite for `precede` parser.

---
+++ test/unit/precede.ts
@@ -0,0 +1,39 @@
+import { Borrow, StringSlice, tag, precede } from "../../src/index";
+import { assert } from "chai";
+
+describe("Parser: precede", () => {
+    let input: StringSlice;
+
+    beforeEach(function () {
+        input = new StringSlice(new Borrow("abcdefghi"));
+    });
+
+    describe("parse", () => {
+        it("should consume `def` preceded by  `abc`", () => {
+            const def = precede(tag("abc"), tag("def"));
+
+            assert.deepEqual(
+                def(input),
+                {
+                    kind: "done",
+                    input: input.splitsAt(6),
+                    output: "def"
+                }
+            );
+        });
+
+        it("should fail to consume `def` preceded by  `xyz`", () => {
+            const def = precede(tag("xyz"), tag("def"));
+
+            assert.deepEqual(
+                def(input),
+                {
+                    kind: "error",
+                    error: {
+                        kind: "tag"
+                    }
+                }
+            );
+        });
+    });
+});
